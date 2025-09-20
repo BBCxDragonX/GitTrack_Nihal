63b37c514a41bded6f65ed83294de6c7f25e1615
Prasoona-carnow <preddy@carnow.com>
2025-09-20 22:59:04 +0530

    MeetCommit

---
+++ src/main/java/com/dreamboat/LeetCode/JavaNewFuck.java
@@ -0,0 +1,16 @@
+package com.dreamboat.LeetCode;
+
+public class JavaNewFuck {
+
+    public void display(){
+        System.out.println("Show me ur body");
+    }
+    public static void main(String[] args){
+        JavaNewFuck js = new JavaNewFuck();
+        js.display();
+        twopeoplFuck();
+    }
+    public static void twopeoplFuck(){
+        System.out.println("Hello let's meet and xxxx");
+    }
+}
